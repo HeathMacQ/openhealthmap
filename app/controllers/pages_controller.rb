f75dac343c9e4e2b42a65a681aca326511176055
heath mac <heathmacq@gmail.com>
2016-08-29 20:49:06 -0700

    prepared website layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,39 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def geography
+  end
+
+  def add
+  end
+
+  def maps
+  end
+
+  def search
+  end
+
+  def about
+  end
+
+  def contribute
+  end
+
+  def project
+  end
+
+  def disease
+  end
+
+  def keywords
+  end
+
+  def build
+  end
+
+  def community
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
 Rails.application.routes.draw do
   root 'pages#home'
+ 
+get "about" => "pages#about.html.erb"
 
-  get "geography" => "pages#geography.html.erb"
+get "add" => "pages#addproject.html.erb"
   
+get "build" => "pages#build.html.erb"
+
+get "community" => "pages#community.html.erb"
+
+get "contribute" => "pages#contribute.html.erb"
+
+get "disease" => "pages#disease.html.erb"
+
+get "geography" => "pages#geography.html.erb"
+
+get "keywords" => "pages#keywords.html.erb"
+
+get "maps" => "pages#maps.html.erb"
+
+get "project" => "pages#project.html.erb"
+
+get "search" => "pages#search.html.erb"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
